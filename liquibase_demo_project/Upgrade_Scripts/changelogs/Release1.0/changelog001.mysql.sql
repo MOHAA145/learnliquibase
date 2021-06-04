e9f9e1761e6d44e91059db0a8528d9e27757a8c1
manishawsfreelancer <manishawsfreelancer@gmail.com>
2021-06-04 16:24:26 +0530

    .

--- liquibase_demo_project/Upgrade_Scripts/changelogs/Release1.0/changelog001.mysql.sql
+++ liquibase_demo_project/Upgrade_Scripts/changelogs/Release1.0/changelog001.mysql.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset dmanishgandhi:JIRA_001
+-- changeset dmanishgandhi:JIRA_0001
 CREATE TABLE `liquibase_testing` (
   `review_id` varchar(32) NOT NULL,
   `order_id` varchar(32) NOT NULL,
